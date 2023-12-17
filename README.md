0013f64afda85b78556e9d203fef8d1b8aeec543
Armand Collin <armac@videotron.ca>
2023-12-17 16:41:55 -0500

    Add setup_nnunet script to preprocess the data

--- README.md
+++ README.md
@@ -8,3 +8,16 @@ This model is trained on 5 datasets. The preprocessing script expects the follow
 - BF dataset (default)
 - BF dataset (wakehealth, human)
 - BF dataset (VCU, rabbit)
+
+First, run the aggregation script:
+```
+python nnunet_scripts -i path_to_directory_containing_dsets -o .
+```
+
+This will create a new nnunet dataset. We can then run the initial setup, 
+move the manual split in the preprocessed folder and start training:
+```
+./setup_nnunet.sh ABSOLUTE_PATH_TO_CWD
+mv final_splits.json nnUNet_preprocessed/Dataset444_AGG
+./train_3fold.sh
+```
---
+++ setup_nnunet.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# This script sets up the nnUNet environment and runs the preprocessing and dataset integrity verification
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 RESULTS_DIR [DATASET_ID] [DATASET_NAME]"
+    exit 1
+fi
+
+config="2d"
+
+RESULTS_DIR=$(realpath $1)
+dataset_id=${3:-444}
+dataset_name=${4:-"AGG"}
+
+echo "-------------------------------------------------------"
+echo "Converting dataset to nnUNetv2 format"
+echo "-------------------------------------------------------"
+
+# Set up the necessary environment variables
+export nnUNet_raw="$RESULTS_DIR/nnUNet_raw"
+export nnUNet_preprocessed="$RESULTS_DIR/nnUNet_preprocessed"
+export nnUNet_results="$RESULTS_DIR/nnUNet_results"
+
+echo "-------------------------------------------------------"
+echo "Running preprocessing and verifying dataset integrity"
+echo "-------------------------------------------------------"
+
+nnUNetv2_plan_and_preprocess -d ${dataset_id} --verify_dataset_integrity -c ${config}
