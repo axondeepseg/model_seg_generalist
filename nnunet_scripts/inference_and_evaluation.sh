b7d636ada6272f0db7f400958f007989a551ad38
Arthur Boschet <arthur.bleu25@gmail.com>
2024-02-13 15:37:00 -0500

    Remove the reliance on the global RESULTS_DIR variable in the inference_and_evalution.sh script

--- nnunet_scripts/inference_and_evaluation.sh
+++ nnunet_scripts/inference_and_evaluation.sh
@@ -4,9 +4,9 @@
 NNUNET_PATH=$1
 DATASET_NAME="Dataset444_AGG"
 
-if [ ! -d "${RESULTS_DIR}/nnUNet_results/${DATASET_NAME}/scores" ]; then
-  echo "Making directory ${RESULTS_DIR}/nnUNet_results/${DATASET_NAME}/scores" 
-  mkdir "${RESULTS_DIR}/nnUNet_results/${DATASET_NAME}/scores"
+if [ ! -d "${NNUNET_PATH}/nnUNet_results/${DATASET_NAME}/scores" ]; then
+  echo "Making directory ${NNUNET_PATH}/nnUNet_results/${DATASET_NAME}/scores" 
+  mkdir "${NNUNET_PATH}/nnUNet_results/${DATASET_NAME}/scores"
 fi
 
 shift
@@ -14,16 +14,16 @@ for dataset in "$@"; do
   echo -e "\n\nRun inference with $NNUNET_PATH/$dataset model on $DATASET_NAME dataset\n\n"
   dataset_name=${dataset#Dataset???_}
 
-  python nnunet_scripts/run_inference.py  --path-dataset ${RESULTS_DIR}/nnUNet_raw/${DATASET_NAME}/imagesTs \
-                                          --path-out ${RESULTS_DIR}/nnUNet_results/${DATASET_NAME}/${dataset_name}_model_best_checkpoints_inference \
+  python nnunet_scripts/run_inference.py  --path-dataset ${NNUNET_PATH}/nnUNet_raw/${DATASET_NAME}/imagesTs \
+                                          --path-out ${NNUNET_PATH}/nnUNet_results/${DATASET_NAME}/${dataset_name}_model_best_checkpoints_inference \
                                           --path-model ~/data/nnunet_all/nnUNet_results/${dataset}/nnUNetTrainer__nnUNetPlans__2d/ \
                                           --use-gpu \
                                           --use-best-checkpoint
 
-  python nnunet_scripts/run_evaluation.py --pred_path ${RESULTS_DIR}/nnUNet_results/${DATASET_NAME}/${dataset_name}_model_best_checkpoints_inference \
-                                          --mapping_path ${RESULTS_DIR}/nnUNet_raw/${DATASET_NAME}/fname_mapping.json \
-                                          --gt_path ${RESULTS_DIR}/test_labels \
-                                          --output_fname ${RESULTS_DIR}/nnUNet_results/${DATASET_NAME}/scores/${dataset_name}_model_best_checkpoints_scores \
+  python nnunet_scripts/run_evaluation.py --pred_path ${NNUNET_PATH}/nnUNet_results/${DATASET_NAME}/${dataset_name}_model_best_checkpoints_inference \
+                                          --mapping_path ${NNUNET_PATH}/nnUNet_raw/${DATASET_NAME}/fname_mapping.json \
+                                          --gt_path ${NNUNET_PATH}/test_labels \
+                                          --output_fname ${NNUNET_PATH}/nnUNet_results/${DATASET_NAME}/scores/${dataset_name}_model_best_checkpoints_scores \
                                           --pred_suffix _0000
 
 done
